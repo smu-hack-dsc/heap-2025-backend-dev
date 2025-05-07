24564e7115b9b06d4d93cbf7286a94bafd1b9b28
Terris <27739101+Terristwj@users.noreply.github.com>
2025-05-07 18:53:36 +0800

    feat: Project organization by topic

---
+++ 1_introduction/demos/demo_script.js
@@ -0,0 +1,5 @@
+const school = "SMU";
+const faculty = "SCIS";
+const CCA = "dotHack";
+
+console.log(`Welcome to ${school} ${faculty}, ${CCA} CCA!`);
---
+++ 1_introduction/demos/demo_server.js
@@ -0,0 +1,18 @@
+// (1) Imports http from the global scope
+const http = require("http");
+
+// (2) Creates a server
+const server = http.createServer((req, res) => {
+    res.setHeader("Content-Type", "application/json");
+
+    // Business logic goes here
+    const myMessage = "Hello, World!";
+
+    // Response
+    res.end(JSON.stringify({ message: myMessage }));
+});
+
+// (3) Starts the server on port 3000
+server.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
